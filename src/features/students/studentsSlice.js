5182ab9111fd932bb395fb1345711f7a1e0d8375
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-07-02 00:30:43 -0400

    Get the student listing from the database

---
+++ src/features/students/studentsSlice.js
@@ -0,0 +1,23 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchStudents = createAsyncThunk(
+  "campuses/fetchStudents",
+  async () => {
+    const response = await axios.get("http://localhost:8080/api/students");
+    return response.data;
+  }
+);
+
+const campusesSlice = createSlice({
+  name: "students",
+  initialState: [],
+  reducers: {},
+  extraReducers: (builder) => {
+     builder.addCase(fetchStudents.fulfilled, (state, action) => {
+      return action.payload;
+      });
+  },
+});
+
+export default campusesSlice.reducer;
