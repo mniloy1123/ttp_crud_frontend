329c682e2bec984ff75c4d75b97ba8ea0a4c69a8
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-07-02 14:17:27 -0400

    Add styling to the campus cards

--- src/features/campuses/Campuses.jsx
+++ src/features/campuses/Campuses.jsx
@@ -8,6 +8,8 @@ import {
   CardMedia,
   Button,
   Typography,
+  Grid,
+  Box,
 } from "@mui/material";
 
 const Campuses = () => {
@@ -20,30 +22,55 @@ const Campuses = () => {
 
   return (
     <div>
-      <h1>Campus Listing</h1>
-      {campuses.map((campus) => (
-        <Card key={campus.id} sx={{ maxWidth: 345 }}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              height="140"
-              image={campus.imageUrl}
-              alt={campus.name}
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {campus.name}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-          <Button size="small" color="primary">
-            Edit
-          </Button>
-          <Button size="small" color="secondary">
-            Delete
-          </Button>
-        </Card>
-      ))}
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        margin={2}
+      >
+        <Typography variant="h4" component="div">
+          Campus Listing
+        </Typography>
+        <Button variant="contained" color="primary">
+          Add Campus
+        </Button>
+      </Box>
+      <Grid container spacing={2}>
+        {campuses.map((campus) => (
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            md={4}
+            lg={3}
+            xl={2}
+            marginLeft={2}
+            key={campus.id}
+          >
+            <Card sx={{ maxWidth: 345 }}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image={campus.imageUrl}
+                  alt={campus.name}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {campus.name}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <Button size="small" color="primary">
+                Edit
+              </Button>
+              <Button size="small" color="error">
+                Delete
+              </Button>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 };
