199cc9687af5706220fcca588a328fc19f0513fe
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-07-02 00:33:09 -0400

    Add each student to a card component

--- src/features/students/Students.jsx
+++ src/features/students/Students.jsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchStudents } from "./studentsSlice";
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@mui/material";
 
 const Students = () => {
+  const dispatch = useDispatch();
+  const students = useSelector((state) => state.students);
+
+  useEffect(() => {
+    dispatch(fetchStudents());
+  }, [dispatch]);
+
   return (
     <div>
-      <h1>Students Page</h1>
+      <h1>All Students</h1>
+      {students.map((student) => (
+        <Card key={student.id} sx={{ maxWidth: 250 }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image={student.imageUrl}
+              alt={student.firstName + " " + student.lastName}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {student.firstName + " " + student.lastName}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <Button size="small" color="primary">
+            Edit
+          </Button>
+          <Button size="small" color="secondary">
+            Delete
+          </Button>
+        </Card>
+      ))}
     </div>
   );
 };
