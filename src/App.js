faf2e9c5e1c9b3dbb339613dc3338d6a120ea915
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-07-02 00:09:50 -0400

    Prettier

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import store from './app/store'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import Campuses from './features/campuses/Campuses';
-import Students from './features/students/Students';
+import React from "react";
+import { Provider } from "react-redux";
+import store from "./app/store";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Campuses from "./features/campuses/Campuses";
+import Students from "./features/students/Students";
 
 function App() {
   return (
