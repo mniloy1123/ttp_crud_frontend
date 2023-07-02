e6c829fa1c4ed967bc059fcb8d05b4f318184cca
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-07-01 23:12:24 -0400

    Add routes to the different pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from "react";
-import { Provider } from "react-redux";
-import store from "./app/store";
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './app/store'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
 import Campuses from './features/campuses/Campuses';
-import "./App.css";
+import Students from './features/students/Students';
 
 function App() {
   return (
     <Provider store={store}>
-      <Campuses />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/campuses" element={<Campuses />} />
+          <Route path="/students" element={<Students />} />
+        </Routes>
+      </Router>
     </Provider>
   );
 }
