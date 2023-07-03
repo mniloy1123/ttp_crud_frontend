178707eabc88b69ab06510f8c9aaff4678b0ac1f
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-07-03 13:24:00 -0400

    Send delete request & add a case for delete.fullfilled to remove the campus from the state

--- src/features/campuses/campusesSlice.js
+++ src/features/campuses/campusesSlice.js
@@ -1,21 +1,24 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const fetchCampuses = createAsyncThunk(
-  "campuses/fetchCampuses",
-  async () => {
-    const response = await axios.get("http://localhost:8080/api/campuses");
-    return response.data;
-  }
-);
+export const fetchCampuses = createAsyncThunk("campuses/fetchCampuses", async () => {
+  const response = await axios.get("http://localhost:8080/api/campuses");
+  return response.data;
+});
 
-export const addCampus = createAsyncThunk(
-  "campuses/addCampus",
-  async (campus) => {
-    const response = await axios.post("http://localhost:8080/api/campuses", campus);
-    return response.data;
+export const addCampus = createAsyncThunk("campuses/addCampus", async (campus) => {
+  const response = await axios.post("http://localhost:8080/api/campuses", campus);
+  return response.data;
+});
+
+export const deleteCampus = createAsyncThunk("campuses/deleteCampus", async (id, { rejectWithValue }) => {
+  try {
+    await axios.delete(`http://localhost:8080/api/campuses/${id}`);
+    return id;
+  } catch (error) {
+    return rejectWithValue(error.response.data);
   }
-);
+});
 
 const campusesSlice = createSlice({
   name: "campuses",
@@ -28,8 +31,10 @@ const campusesSlice = createSlice({
     builder.addCase(addCampus.fulfilled, (state, action) => {
       state.push(action.payload);
     });
+    builder.addCase(deleteCampus.fulfilled, (state, action) => {
+      return state.filter((campus) => campus.id !== action.payload);
+    });
   },
 });
-
 
 export default campusesSlice.reducer;
