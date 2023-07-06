3180056ab2f1ea877d628cfb3af27a0171ca7db1
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-07-06 14:50:42 -0400

    Implement dropdown menu to add students associated with the campus

--- src/features/campuses/EditCampusPage.jsx
+++ src/features/campuses/EditCampusPage.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import { TextField, Typography, Button, Box, Select, MenuItem } from "@mui/material";
 import { fetchSingleCampus, updateCampus } from "./campusesSlice";
-//import { fetchStudents, updateStudent } from "../students/studentsSlice";
+import { fetchStudents, updateStudent } from "../students/studentsSlice";
 
 const EditCampusPage = () => {
   const { id } = useParams();
@@ -10,24 +11,23 @@ const EditCampusPage = () => {
   const dispatch = useDispatch();
 
   const campus = useSelector((state) => state.campuses.singleCampus); //singleCampus
-  //const allStudents = useSelector((state) => state.students.list); //list
+  const allStudents = useSelector((state) => state.students.list); //list
 
-  const [campusInfo, setCampusInfo] = useState({ name: '', imageUrl: '', address: '', description: '' });
+  const [campusInfo, setCampusInfo] = useState({ name: "", imageUrl: "", address: "", description: "" });
+  const [selectedStudentId, setSelectedStudentId] = useState("");
 
-useEffect(() => {
+  useEffect(() => {
     dispatch(fetchSingleCampus(id));
-}, [dispatch, id]);
+    dispatch(fetchStudents());
+  }, [dispatch, id]);
 
-useEffect(() => {
+  useEffect(() => {
     setCampusInfo(campus);
   }, [campus]);
   useEffect(() => {
     console.log("campusInfo has changed: ", campusInfo);
   }, [campusInfo]);
 
- // useEffect(() => {
-  //  dispatch(fetchStudents());
- // }, [dispatch]);
 
   const handleChange = (event) => {
     setCampusInfo({ ...campusInfo, [event.target.name]: event.target.value });
@@ -40,39 +40,76 @@ useEffect(() => {
     navigate(`/campuses/`); // Navigate back to the campus page after updating the campus.
   };
 
-  //const handleAddStudent = (studentId) => {
- //   const student = allStudents.find((student) => student.id === studentId);
- //   dispatch(updateStudent({ ...student, campusId: campus.id })); // Update the student with the new campus ID.
-//  };
+  const handleAddStudent = (studentId) => {
+    const student = allStudents.find((student) => student.id === studentId);
+    dispatch(updateStudent({ ...student, campusId: campus.id })); // Update the student with the new campus ID.
+  };
 
   return (
     <div>
-      <h1>Edit Campus</h1>
-      <form>
-        <input name='name' value={campusInfo.name} onChange={handleChange} placeholder='Name' />
-        <input name='imageUrl' value={campusInfo.imageUrl} onChange={handleChange} placeholder='Image URL' />
-        <input name='address' value={campusInfo.address} onChange={handleChange} placeholder='Address' />
-        <textarea name='description' value={campusInfo.description} onChange={handleChange} placeholder='Description' />
-        {/* Button for submitting the form. */}
-        <button type='button' onClick={handleUpdate}>Save Changes</button>
+      <Box display="flex" justifyContent="center" m={2}>
+        <Typography variant="h4" component="div">
+          Edit Campus
+        </Typography>
+      </Box>
+      <form noValidate>
+        <Box sx={{ display: "flex", flexDirection: "column", m: 4 }}>
+          <TextField
+            margin="normal"
+            name="name"
+            value={campusInfo.name}
+            onChange={handleChange}
+            label="College Name"
+            variant="outlined"
+            required
+          />
+          <TextField
+            margin="normal"
+            name="imageUrl"
+            value={campusInfo.imageUrl}
+            onChange={handleChange}
+            label="Image URL"
+          />
+          <TextField
+            margin="normal"
+            name="address"
+            value={campusInfo.address}
+            onChange={handleChange}
+            label="Address"
+          />
+          <TextField
+            margin="normal"
+            name="description"
+            required
+            value={campusInfo.description}
+            onChange={handleChange}
+            label="Description"
+            multiline
+            rows={4}
+          />
+          {/* Button for submitting the form. */}
+          <Button sx={{ mx: "auto", mt: 2 }} variant="contained" color="primary" onClick={handleUpdate}>
+            Save Changes
+          </Button>
+        </Box>
       </form>
-         {/* Dropdown for selecting a student to add to the campus. 
-      <div>
-        <h2>Students on Campus</h2>
-     
-        <select onChange={(event) => handleAddStudent(event.target.value)}>
+      <Box m={2}>
+        <Typography variant="h6" component="div">
+          Add a Student
+        </Typography>
+        <Select value={selectedStudentId} onChange={(event) => setSelectedStudentId(event.target.value)}>
           {allStudents.map((student) => (
-            <option value={student.id} key={student.id}>
+            <MenuItem value={student.id} key={student.id}>
               {student.firstName} {student.lastName}
-            </option>
+            </MenuItem>
           ))}
-        </select>
-        <button type='button' onClick={() => {}}>Add to Campus</button>
-      </div> */}
+        </Select>
+        <Button sx={{ mx: "auto", mt: 2 }} variant="contained" color="primary" onClick={() => handleAddStudent(selectedStudentId)}>
+          Add Student
+        </Button>
+      </Box>
     </div>
   );
-
-
 };
 
 export default EditCampusPage;
