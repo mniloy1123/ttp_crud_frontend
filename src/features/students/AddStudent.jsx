f5f0ec19d48a75fc4453216a5fa241040b8a822e
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-07-06 13:23:33 -0400

    Add constraints to the form and button

--- src/features/students/AddStudent.jsx
+++ src/features/students/AddStudent.jsx
@@ -13,6 +13,13 @@ const AddStudent = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
+  const gpaNumber = parseFloat(gpa);
+  const isGpaValid = !isNaN(gpaNumber) && gpaNumber >= 0.0 && gpaNumber <= 4.0;
+  const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+  const isEmailValid = emailRegex.test(email);
+  const isFormValid =
+    firstName.trim() !== "" && imageUrl.trim() !== "" && lastName.trim() !== "" && isEmailValid && isGpaValid;
+
   const handleSubmit = (e) => {
     e.preventDefault();
     dispatch(addStudent({ firstName, lastName, email, imageUrl, gpa }));
@@ -22,12 +29,42 @@ const AddStudent = () => {
   return (
     <form onSubmit={handleSubmit}>
       <Box sx={{ display: "flex", flexDirection: "column", m: 4 }}>
-        <TextField margin="normal" required label="First Name" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-        <TextField margin="normal" required label="Last Name" value={lastName} onChange={(e) => setLastName(e.target.value)} />
-        <TextField margin="normal" required label="Email Address" value={email} onChange={(e) => setEmail(e.target.value)} />
-        <TextField margin="normal" label="Image Url" value={imageUrl} onChange={(e) => setImageUrl(e.target.value)} />
-        <TextField margin="normal" required label="GPA" value={gpa} onChange={(e) => setGpa(e.target.value)} />
-        <Button sx={{ mx: "auto", mt: 2 }} variant="contained" type="submit">
+        <TextField
+          margin="normal"
+          required
+          label="First Name"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <TextField
+          margin="normal"
+          required
+          label="Last Name"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        <TextField
+          margin="normal"
+          required
+          label="Email Address"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+          margin="normal"
+          required
+          label="Image Url"
+          value={imageUrl}
+          onChange={(e) => setImageUrl(e.target.value)}
+        />
+        <TextField
+          margin="normal"
+          required
+          label="GPA (0.0-4.0)"
+          value={gpa}
+          onChange={(e) => setGpa(e.target.value)}
+        />
+        <Button sx={{ mx: "auto", mt: 2 }} variant="contained" type="submit" disabled={!isFormValid}>
           Add Student
         </Button>
       </Box>
