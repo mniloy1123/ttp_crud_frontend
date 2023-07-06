86fd01d53290b6a75e09e26d9d92ef8722582dc5
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-07-06 00:40:16 -0400

    Add styling to make the inputs appear in a column and space in between them

--- src/features/students/EditStudentPage.jsx
+++ src/features/students/EditStudentPage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchSingleStudent, updateStudent } from "./studentsSlice";
-import { TextField, Button } from "@mui/material";
+import { TextField, Typography, Button, Box } from "@mui/material";
 
 const EditStudentPage = () => {
   const { id } = useParams();
@@ -45,50 +45,66 @@ const EditStudentPage = () => {
       await dispatch(updateStudent({ id: student.id, student: studentInfo }));
       console.log("After update: ", studentInfo);
       navigate(`/students/`);
-    } 
+    }
   };
 
   return (
     <div>
-      <h1>Edit Student</h1>
+      <Box display="flex" justifyContent="center" m={2}>
+        <Typography variant="h4" component="div">
+          Edit Student
+        </Typography>
+      </Box>
       <form noValidate>
-        <TextField
-          name="firstName"
-          value={studentInfo.firstName}
-          onChange={handleChange}
-          label="First Name"
-          variant="outlined"
-          required
-        />
-        <TextField
-          name="lastName"
-          value={studentInfo.lastName}
-          onChange={handleChange}
-          label="Last Name"
-          variant="outlined"
-          required
-        />
-        <TextField
-          type="number"
-          name="gpa"
-          value={studentInfo.gpa}
-          onChange={handleChange}
-          label="GPA"
-          variant="outlined"
-          required
-          InputProps={{ inputProps: { min: 0, max: 4, step: 0.1 } }}
-        />
-        <TextField
-          name="imageUrl"
-          value={studentInfo.imageUrl}
-          onChange={handleChange}
-          label="Student Image URL"
-          variant="outlined"
-          required
-        />
-        <Button variant="contained" color="primary" onClick={handleUpdate} disabled={!isFormValid}>
-          Save Changes
-        </Button>
+        <Box sx={{ display: "flex", flexDirection: "column", m: 4 }}>
+          <TextField
+            margin="normal"
+            name="firstName"
+            value={studentInfo.firstName}
+            onChange={handleChange}
+            label="First Name"
+            variant="outlined"
+            required
+          />
+          <TextField
+            margin="normal"
+            name="lastName"
+            value={studentInfo.lastName}
+            onChange={handleChange}
+            label="Last Name"
+            variant="outlined"
+            required
+          />
+          <TextField
+            margin="normal"
+            type="number"
+            name="gpa"
+            value={studentInfo.gpa}
+            onChange={handleChange}
+            label="GPA"
+            variant="outlined"
+            required
+            InputProps={{ inputProps: { min: 0, max: 4, step: 0.1 } }}
+          />
+          <TextField
+            margin="normal"
+            name="imageUrl"
+            value={studentInfo.imageUrl}
+            onChange={handleChange}
+            label="Student Image URL"
+            variant="outlined"
+            required
+          />
+          <Button
+            sx={{ mx: "auto", mt: 2 }}
+            variant="contained"
+            color="primary"
+            onClick={handleUpdate}
+            disabled={!isFormValid}
+          >
+            Save Changes
+          </Button>
+        </Box>
       </form>
     </div>
   );
