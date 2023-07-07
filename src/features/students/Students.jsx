4c68f78983e69db889b337d253a8210714c61314
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-07-07 01:25:39 -0400

    Use StudentCard component

--- src/features/students/Students.jsx
+++ src/features/students/Students.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { fetchStudents } from "./studentsSlice";
 import { fetchCampuses } from "../campuses/campusesSlice";
 import { useGetCampusName, useHandleCardClick} from "../../utils/utils";
+import StudentCard from "../../components/StudentCard"
 import { Card, CardActionArea, CardContent, CardMedia, Button, Typography, Grid, Box } from "@mui/material";
 
 const Students = () => {
@@ -23,39 +24,25 @@ const Students = () => {
 
   return (
     <div>
-      <Box display="flex" justifyContent="space-between" alignItems="center" margin={2}>
-        <Typography variant="h4" component="div">
-          All Students
-        </Typography>
-        <Button variant="contained" color="primary" onClick={() => navigate("/add-student")}>
-          Add Student
-        </Button>
-      </Box>
-      <Grid container spacing={2}>
-        {students.map((student) => (
-          <Grid item xs={12} sm={6} md={4} lg={3} xl={2} marginLeft={2} key={student.id}>
-            <Card onClick={() => handleCardClick(student.id)} sx={{ maxWidth: 250, minHeight: 265 }}>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image={student.imageUrl}
-                  alt={student.firstName + " " + student.lastName}
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    {student.firstName + " " + student.lastName}
-                  </Typography>
-                  <Typography variant="body2" color="textSecondary">
-                    Campus: {getCampusName(student.campusId)}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
-    </div>
+    <Box display="flex" justifyContent="space-between" alignItems="center" margin={2}>
+      <Typography variant="h4" component="div">
+        All Students
+      </Typography>
+      <Button variant="contained" color="primary" onClick={() => navigate("/add-student")}>
+        Add Student
+      </Button>
+    </Box>
+    <Grid container spacing={2}>
+      {students.map((student) => (
+        <StudentCard 
+          key={student.id}
+          student={student}
+          getCampusName={getCampusName}
+          handleCardClick={handleCardClick}
+        />
+      ))}
+    </Grid>
+  </div>
   );
 };
 
