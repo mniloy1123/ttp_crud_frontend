da47dea9ed9d3a455e35088ca4f594c9d738f432
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-07-07 02:52:22 -0400

    Homepage and navbar styling

--- src/App.js
+++ src/App.js
@@ -12,25 +12,46 @@ import SingleStudentPage from "./features/students/SingleStudentPage";
 import EditCampusPage from "./features/campuses/EditCampusPage";
 import EditStudentPage from "./features/students/EditStudentPage";
 import AddStudent from "./features/students/AddStudent";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: '"Martel", serif',
+    h1: {
+      fontFamily: '"Source Code Pro", monospace',
+    },
+    h2: {
+      fontFamily: '"Source Code Pro", monospace',
+    },
+    h3: {
+      fontFamily: '"Source Code Pro", monospace',
+    },
+    h4: {
+      fontFamily: '"Source Code Pro", monospace',
+    },
+  },
+});
 
 function App() {
   return (
-    <Provider store={store}>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/campuses" element={<Campuses />} />
-          <Route path="/students" element={<Students />} />
-          <Route path="/add-campus" element={<AddCampus />} />
-          <Route path="/campuses/:id" element={<SingleCampusPage />} />
-          <Route path="/campuses/:id/edit" element={<EditCampusPage />} />
-          <Route path="/students/:id" element={<SingleStudentPage />} />
-          <Route path="/students/:id/edit" element={<EditStudentPage />} />
-          <Route path="/add-student" element={<AddStudent />} />
-        </Routes>
-      </Router>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/campuses" element={<Campuses />} />
+            <Route path="/students" element={<Students />} />
+            <Route path="/add-campus" element={<AddCampus />} />
+            <Route path="/campuses/:id" element={<SingleCampusPage />} />
+            <Route path="/campuses/:id/edit" element={<EditCampusPage />} />
+            <Route path="/students/:id" element={<SingleStudentPage />} />
+            <Route path="/students/:id/edit" element={<EditStudentPage />} />
+            <Route path="/add-student" element={<AddStudent />} />
+          </Routes>
+        </Router>
+      </Provider>
+    </ThemeProvider>
   );
 }
 
